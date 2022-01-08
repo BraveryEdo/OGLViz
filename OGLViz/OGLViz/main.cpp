bb3e17296b6de37cdb87b11f3779269b8b811683
BraveryEdo <Braveryedo@gmail.com>
2022-01-07 20:12:57 -0500

    ignore test

--- OGLViz/OGLViz/main.cpp
+++ OGLViz/OGLViz/main.cpp
@@ -1,18 +1,66 @@
 #include "windows.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <GLM\glm\glm.hpp>
 #include <GL\glut.h>
+using namespace glm;
 
 int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	glutCreateWindow("GLEW Test");
-	GLenum err = glewInit();
-	if (GLEW_OK != err)
+	fprintf(stdout, "Visualizer Project by LiquidState, C++ build utilizing OpenGL");//\nUsing GLEW version: %s\n", glewGetString(GLEW_VERSION));
+	
+	// Initialise GLFW
+	glewExperimental = true; // Needed for core profile
+	if (!glfwInit())
 	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
 	}
-	fprintf(stdout, "Status: Using GLEW version: %s\n", glewGetString(GLEW_VERSION));
+	
+	//GLFW initial settings
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+	// Open a window and create its OpenGL context
+	GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Initialize GLEW
+	glewExperimental = true; // Needed in core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+	//VAO (vertex Array Obj) needs to be right after window creation
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw nothing, see you in tutorial 2 !
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
 
 	return 0;
-} 
+}  
